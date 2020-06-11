d669b7d44ce8a94f6345e55b82c33c80068af3ce
thually <alejosalazarmejia.2000@gmail.com>
2020-06-11 16:56:09 -0500

    This is the cachefile for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that compute and cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## First, let's initialize the variables x (a matrix)
+  ## and inverse (it's inverse)
+  inverse <- NULL
+  ## Let's create a function that can get the value of the matrix.
+  getM <- function() x
+  ## Create a function that can cache the inverse of a matrix.
+  setInverse <- function(inv) inverse <<- inv ## Again, we use the 
+  ## operator for caching the new value of 'inverse' in the parental
+  ## environment.
+  ## Our last function is going to get the inverse of the matrix
+  getInverse <- function() inverse
+  ##Finaly, our function return a list of the previous functions 
+  ## so later we can access any object defined in the environment of 
+  ## 'makeCacheMatrix'
+  list(getM = getM, setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse() ## initializing a variable with the currrent value 
+  ## of the inverse of 'x'
+  
+  if(!is.null(inverse)) { ## Checks if the inverse has already been calculated
+    message("getting cached data") ## if so, return the value of inverse
+    return(inverse)
+  }
+  ## Else, we use the matrix 'x' to calculate and return its inverse
+  myMatrix <- x$getM()
+  inverse <- solve(myMatrix, ...) ## we use the '<-0 operator because we are updating
+  ## a variable that already exists in this environment
+  x$setInverse(inverse)
+  inverse
 }
